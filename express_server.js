ff81cda99331ebf68a2f17d3cc8be35cca3a1ea8
Nelli Butenko <butenkonelly93@gmail.com>
2021-04-12 19:53:13 +0000

    urls_index Template filled out

--- express_server.js
+++ express_server.js
@@ -13,9 +13,16 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase }
+  res.render('urls_index', templateVars); // to pass the URL data to our template.
 });
+
+/*
+Need to send them inside an object, even if we are only sending one variable.
+This is so we can use the key of that variable (in the above case the key is urls)
+to access the data within our template.
+*/
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
